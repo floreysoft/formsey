b5f7bb11704d978b9022676973b158da19bcf3c3
dflorey <daniel.florey@gmail.com>
2020-02-27 13:55:32 +0100

    make full width

--- packages/formsey-fields-material/StringField.ts
+++ packages/formsey-fields-material/StringField.ts
@@ -1,12 +1,12 @@
 import "@material/mwc-textfield/mwc-textfield.js";
 import { TextField } from "@material/mwc-textfield/mwc-textfield.js";
-import { Field, StringFieldDefinition } from '@formsey/core';
+import { Field, StringFieldDefinition, LabeledField } from '@formsey/core';
 import { InvalidEvent } from '@formsey/core/InvalidEvent';
 import { customElement, html, property, query } from 'lit-element';
 import { ifDefined } from 'lit-html/directives/if-defined.js';
 
 @customElement("formsey-string-material")
-export class StringField extends Field<StringFieldDefinition, string> {
+export class StringField extends LabeledField<StringFieldDefinition, string> {
   @property({ type: String })
   value: string;
 
@@ -14,7 +14,7 @@ export class StringField extends Field<StringFieldDefinition, string> {
   materialTextField : TextField
 
   renderField() {
-    return html`<mwc-textfield style="display:flex" label="${this.definition.prompt}" helper="${this.definition.helpText}" ?autofocus="${this.definition.focus}" ?required="${this.definition.required}" autocomplete="${ifDefined(this.definition.autofill)}" @input="${this.valueChanged}" name="${this.definition.name}" placeholder="${ifDefined(this.definition.placeholder)}" .maxlength="${ifDefined(this.definition.maxlength)}" .value="${ifDefined(this.value)}"></mwc-textfield>`;
+    return html`<mwc-textfield style="display:flex" fullwidth="true" helper="${this.definition.helpText}" ?autofocus="${this.definition.focus}" ?required="${this.definition.required}" autocomplete="${ifDefined(this.definition.autofill)}" @input="${this.valueChanged}" name="${this.definition.name}" placeholder="${ifDefined(this.definition.placeholder)}" .maxlength="${ifDefined(this.definition.maxlength)}" .value="${ifDefined(this.value)}"></mwc-textfield>`;
   }
 
   checkValidity() {
