45135e891fdb902fe6ec5441ddbdbbc11810cbb6
dflorey <daniel.florey@gmail.com>
2020-03-05 12:31:23 +0100

    fix checkbox

--- packages/formsey-fields-material/BooleanField.ts
+++ packages/formsey-fields-material/BooleanField.ts
@@ -1,5 +1,5 @@
 import { customElement, html, property, query } from 'lit-element';
-import { BooleanFieldDefinition, LabeledField } from '@formsey/core';
+import { BooleanFieldDefinition, LabeledField, ValueChangedEvent } from '@formsey/core';
 import "@material/mwc-checkbox/mwc-checkbox.js";
 import { Checkbox } from "@material/mwc-checkbox/mwc-checkbox.js";
 
@@ -13,5 +13,12 @@ export class BooleanField extends LabeledField<BooleanFieldDefinition, boolean> 
 
   renderField() {
     return html`<mwc-checkbox @click="${(event) => this.valueChanged(event)}" .indeterminate="${this.definition.indeterminate}" .value="${this.value}"></mwc-checkbox>`;
+  }
+
+  protected valueChanged(e: any) {
+    this.value = this.materialCheckbox.checked
+    if (this.definition.name) {
+      this.dispatchEvent(new ValueChangedEvent(this.definition.name, this.value));
+    }
   }
 }
