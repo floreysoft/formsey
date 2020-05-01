57928dc76fa9d0b891e2818aa3058bed33fc8015
dflorey <daniel.florey@gmail.com>
2020-05-01 14:54:21 +0200

    test

---
+++ packages/formsey-fields-material/FormField.ts
@@ -0,0 +1,22 @@
+import { FormField as CoreFormField, register } from '@formsey/core';
+import { css } from 'lit-element';
+
+export class FormField extends CoreFormField {
+  static get styles() {
+    return [...super.styles, css`
+      .grid {
+        grid-gap: 5px;
+      }
+
+      .title {
+        font-size: 24px;
+      }
+
+      .description {
+        font-size: 18px;
+      }
+      `];
+  }
+
+}
+register('formsey-form-field-material', FormField)
