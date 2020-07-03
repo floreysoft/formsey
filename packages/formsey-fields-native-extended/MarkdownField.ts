c97fdb9d5ae00bd9e860a9a219eb51f7ab5182a9
dflorey <daniel.florey@gmail.com>
2020-07-03 11:46:49 +0200

    styles

--- packages/formsey-fields-native-extended/MarkdownField.ts
+++ packages/formsey-fields-native-extended/MarkdownField.ts
@@ -18,33 +18,36 @@ export class MarkdownField extends LabeledField<FieldDefinition, string> {
   }
 
   static get styles() {
-    return [ css`
-    pre {
-      background-color: var(--fs-panel-background-color);
-      padding: .5em;
-      display:block;
-      overflow-x:auto
-    }
-    .hljs-subst{color:#444}.hljs-comment{color:#888}.hljs-attribute,.hljs-doctag,.hljs-keyword,.hljs-meta-keyword,.hljs-name,.hljs-selector-tag{font-weight:bolder}.hljs-deletion,.hljs-number,.hljs-quote,.hljs-selector-class,.hljs-selector-id,.hljs-string,.hljs-template-tag,.hljs-type{color:#800}.hljs-section,.hljs-title{color:#800;font-weight:700}.hljs-link,.hljs-regexp,.hljs-selector-attr,.hljs-selector-pseudo,.hljs-symbol,.hljs-template-variable,.hljs-variable{color:#bc6060}.hljs-literal{color:#78a960}.hljs-addition,.hljs-built_in,.hljs-bullet,.hljs-code{color:#397300}.hljs-meta{color:#1f7199}.hljs-meta-string{color:#4d99bf}.hljs-emphasis{font-style:italic}.hljs-strong{font-weight:bold}
-    ` ]
+    return [css`
+      pre {
+        background-color: var(--fs-panel-background-color);
+        border-radius: var(--fs-border-radius);
+        border: 1px solid var(--fs-border-color);
+        padding: .5em;
+        display:block;
+        overflow-x:auto;
+        white-space: pre-wrap;
+      }
+      .hljs-subst{color:#444}.hljs-comment{color:#888}.hljs-attribute,.hljs-doctag,.hljs-keyword,.hljs-meta-keyword,.hljs-name,.hljs-selector-tag{font-weight:bolder}.hljs-deletion,.hljs-number,.hljs-quote,.hljs-selector-class,.hljs-selector-id,.hljs-string,.hljs-template-tag,.hljs-type{color:#800}.hljs-section,.hljs-title{color:#800;font-weight:700}.hljs-link,.hljs-regexp,.hljs-selector-attr,.hljs-selector-pseudo,.hljs-symbol,.hljs-template-variable,.hljs-variable{color:#bc6060}.hljs-literal{color:#78a960}.hljs-addition,.hljs-built_in,.hljs-bullet,.hljs-code{color:#397300}.hljs-meta{color:#1f7199}.hljs-meta-string{color:#4d99bf}.hljs-emphasis{font-style:italic}.hljs-strong{font-weight:bold}
+      ` ]
   }
 
-  private markup : string
-  private _definition : FieldDefinition
+  private markup: string
+  private _definition: FieldDefinition
 
   constructor() {
     super()
     Marked.setOptions({
-        renderer: new Renderer,
-        gfm: true,
-        tables: true,
-        breaks: false,
-        pedantic: false,
-        sanitize: false,
-        smartLists: true,
-        smartypants: false,
-        highlight: (code, lang) => hljs.highlightAuto(code).value
-      });
+      renderer: new Renderer,
+      gfm: true,
+      tables: true,
+      breaks: false,
+      pedantic: false,
+      sanitize: false,
+      smartLists: true,
+      smartypants: false,
+      highlight: (code, lang) => hljs.highlightAuto(code).value
+    });
   }
 
   renderField() {
