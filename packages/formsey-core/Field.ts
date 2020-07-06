b575fcd3c07743cf7e359b3678f74a0d6727d5b4
Alexander Brickwedde <alex_git@brickwedde.de>
2020-07-06 14:24:15 +0200

    path corrected

--- packages/formsey-core/Field.ts
+++ packages/formsey-core/Field.ts
@@ -4,8 +4,8 @@ import { ValueChangedEvent } from './ValueChangedEvent';
 import { FieldDefinition, InputFieldDefinition } from './FieldDefinitions';
 import { InvalidError, InvalidErrors } from './InvalidEvent';
 import { ClickEvent } from './ClickEvent';
-import { BlurEvent } from '../formsey';
-import { FocusEvent } from '../formsey';
+import { BlurEvent } from './BlurEvent';
+import { FocusEvent } from './FocusEvent';
 
 export function hacktml(parts, ...args) {
   const newArgs = args.concat().slice(1, -1)
