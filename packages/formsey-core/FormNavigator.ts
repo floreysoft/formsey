0a3e5a2a37e789644733c06a44c115426d13d62f
dflorey <daniel.florey@gmail.com>
2020-07-16 15:38:15 +0200

    field nav started

--- packages/formsey-core/FormNavigator.ts
+++ packages/formsey-core/FormNavigator.ts
@@ -5,6 +5,13 @@ import { InvalidError, InvalidErrors } from "./InvalidEvent";
 import { get } from "./Form";
 import { FormDefinition, OptionalSectionFieldDefinition, SelectableSectionFieldDefinition } from "./FieldDefinitions";
 
+interface FieldInfo {
+  filled: boolean
+  required: boolean
+  path: string
+  focused: boolean
+}
+
 export class FormNavigator extends LitElement {
   @property({ converter: Object })
   components: Components
@@ -26,10 +33,16 @@ export class FormNavigator extends LitElement {
   focusedPath: string
 
   @query("#prev")
-  previous: HTMLButtonElement
+  previousError: HTMLButtonElement
 
   @query("#next")
-  next: HTMLButtonElement
+  nextError: HTMLButtonElement
+
+  @query("#prevRequired")
+  previousRequired: HTMLButtonElement
+
+  @query("#nextRequired")
+  nextRequired: HTMLButtonElement
 
   @query(".focused")
   focused: HTMLElement
@@ -37,6 +50,8 @@ export class FormNavigator extends LitElement {
   private _errors: InvalidErrors
   private _errorsArray: [string, InvalidError][]
   private focusedError: number = -1
+  private focusedField: number = -1
+  private focusedRequired: number = -1
 
   static get styles() {
     return css`
@@ -52,11 +67,13 @@ export class FormNavigator extends LitElement {
       display: flex;
       flex-direction: row;
       align-items: center;
+      line-height: 1em;
+    }
+    .fieldset .fieldset {
       background-color: #00000011;
       border-radius: 2em;
-      margin: 0 0.15em;
-      padding: 1px 4px;
-      line-height: 0;
+      padding: 0;
+      margin-right: 4px;
     }
     .dots::-webkit-scrollbar {
       width: 0;
@@ -64,9 +81,9 @@ export class FormNavigator extends LitElement {
     }
     .dot {
       cursor: pointer;
-      width: 1em;
-      height: 1em;
-      min-width: 1em;
+      width: 1.5em;
+      height: 1.5em;
+      min-width: 1.5em;
       border-radius: 50%;
       background-color: #a0a0a0;
       margin: .1em;
@@ -90,24 +107,39 @@ export class FormNavigator extends LitElement {
   }
 
   render() {
+    let fields : FieldInfo[] = []
     let dots: TemplateResult[] = []
     if (this.definition) {
-      this.addDots(dots, this.definition)
+      this.addFields(fields, dots, this.definition)
     }
+    let required = 0, requiredFilled = 0, totalFilled = 0
+    fields.forEach(fieldInfo=> {
+      required += fieldInfo.required ? 1 : 0;
+      requiredFilled += fieldInfo.required && fieldInfo.filled ? 1 : 0;
+      totalFilled += fieldInfo.filled ? 1 : 0;
+    })
+    let nav = html`${totalFilled} / ${fields.length}, ${requiredFilled} / ${required} <button id="prevRequired" disabled @click="${e => { this.focusRequired(this.focusedRequired - 1) }}"><</button><button id="nextRequired" disabled @click="${e => { this.focusRequired(this.focusedRequired + 1) }}">></button>`
     let errors = this._errorsArray ?
       html`${this._errorsArray.length} errors <button id="prev" disabled @click="${e => { this.focusError(this.focusedError - 1) }}">Prev</button><button id="next" @click="${e => { this.focusError(this.focusedError + 1) }}">Next</button>` :
       html`No errors`
-    return html`<div class="dots">${dots}<div class="errors">${errors}</div></div>`
+    return html`<div class="dots">${dots}<div class="errors">${nav}${errors}</div></div>`
+  }
+
+  focusRequired(index: number) {
+    this.focusedRequired = index
+    this.previousRequired.disabled = index == 0
+    this.nextError.disabled = index == this._errorsArray.length - 1
+    this.dispatchEvent(new CustomEvent('focusField', { detail: this._errorsArray[index][0] }))
   }
 
   focusError(index: number) {
     this.focusedError = index
-    this.previous.disabled = index == 0
-    this.next.disabled = index == this._errorsArray.length - 1
+    this.previousError.disabled = index == 0
+    this.nextError.disabled = index == this._errorsArray.length - 1
     this.dispatchEvent(new CustomEvent('focusField', { detail: this._errorsArray[index][0] }))
   }
 
-  private addDots(dots: TemplateResult[], fieldDefinition: FieldDefinition, path?: string) {
+  private addFields(fields: FieldInfo[], dots: TemplateResult[], fieldDefinition: FieldDefinition, path?: string) {
     path = path ? path + (fieldDefinition.name ? "." + fieldDefinition.name : '') : fieldDefinition.name
     let nestedDots: TemplateResult[] = []
     if (fieldDefinition.type == "repeatingSection") {
@@ -115,29 +147,28 @@ export class FormNavigator extends LitElement {
       const sections = get(this.value, path)
       if (sections) {
         for (let i = 0; i < sections.length; i++) {
-          this.addDots(nestedDots, fieldDefinition, path + "[" + i + "]")
+          this.addFields(fields, dots, fieldDefinition, path + "[" + i + "]")
         }
       }
-      dots.push(html`<div class="fieldset">${nestedDots}</div>`)
     } else if (fieldDefinition.type == "optionalSection") {
       const checked = get(this.value, path)
       if (checked) {
-        this.addDot(nestedDots, fieldDefinition, path)
+        this.addField(fields, nestedDots, fieldDefinition, path)
         for (let field of ((<OptionalSectionFieldDefinition>fieldDefinition).form).fields) {
-          this.addDots(nestedDots, field, path)
+          this.addFields(fields, nestedDots, field, path)
         }
         dots.push(html`<div class="fieldset">${nestedDots}</div>`)
       } else {
-        this.addDot(dots, fieldDefinition, path)
+        this.addField(fields, dots, fieldDefinition, path)
       }
     } else if (fieldDefinition.type == "selectableSection") {
-      this.addDot(nestedDots, fieldDefinition, path+".selection")
+      this.addField(fields, nestedDots, fieldDefinition, path+".selection")
       let values = (<SelectableSectionFieldDefinition>fieldDefinition).selections.map(selection => (selection.value ? selection.value : selection.label));
       let index = values.indexOf(get(this.value, path+".selection"));
       const selection = (<SelectableSectionFieldDefinition>fieldDefinition).selections[index]
       if (selection) {
         for (let field of selection.form.fields) {
-          this.addDots(nestedDots, field, path+".value")
+          this.addFields(fields, nestedDots, field, path+".value")
         }
       }
       dots.push(html`<div class="fieldset">${nestedDots}</div>`)
@@ -147,20 +178,23 @@ export class FormNavigator extends LitElement {
       }
       if (fieldDefinition.hasOwnProperty('fields')) {
         for (let field of (<FormDefinition>fieldDefinition).fields) {
-          this.addDots(nestedDots, field, path)
+          this.addFields(fields, nestedDots, field, path)
         }
         dots.push(html`<div class="fieldset">${nestedDots}</div>`)
       } else {
         const component = this.components[fieldDefinition.type]
         if (component.focusable) {
-          this.addDot(dots, fieldDefinition, path)
+          this.addField(fields, dots, fieldDefinition, path)
         }
       }
     }
   }
 
-  private addDot(dots: TemplateResult[], fieldDefinition: FieldDefinition, path: string) {
-    dots.push(html`<div class="${classMap({ dot: true, filled: !!get(this.value, path) && !this._errors, invalid: this._errors && !!this._errors.get(path), required: (<InputFieldDefinition>fieldDefinition).required, focused: this.focusedPath == path })}" title="${fieldDefinition.label ? fieldDefinition.label : fieldDefinition.name}" @click="${(e: Event) => { console.log("Dot path=" + path); this.dispatchEvent(new CustomEvent('focusField', { detail: path })) }}"></div>`)
+  private addField(fields: FieldInfo[], dots: TemplateResult[], fieldDefinition: FieldDefinition, path: string) {
+    const focused = this.focusedPath == path
+    const filled = !!get(this.value, path)
+    fields.push({ filled, required: (<InputFieldDefinition>fieldDefinition).required, path, focused })
+    dots.push(html`<div class="${classMap({ dot: true, filled: filled && !this._errors, invalid: this._errors && !!this._errors.get(path), required: (<InputFieldDefinition>fieldDefinition).required, focused })}" title="${fieldDefinition.label ? fieldDefinition.label : fieldDefinition.name}" @click="${(e: Event) => { console.log("Dot path=" + path); this.dispatchEvent(new CustomEvent('focusField', { detail: path })) }}"></div>`)
   }
 }
 register("formsey-form-navigator", FormNavigator)
