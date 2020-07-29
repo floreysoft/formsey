5e90e9e965c6d0c9ec964cfc3d1ab8c4384f4fc3
dflorey <daniel.florey@gmail.com>
2020-07-29 13:42:39 +0200

    removed label from list

--- packages/formsey-fields-native/SelectableSectionField.ts
+++ packages/formsey-fields-native/SelectableSectionField.ts
@@ -26,7 +26,7 @@ export class SelectableSectionField extends LabeledField<SelectableSectionFieldD
       if (selection) {
         let value = selection.value ? selection.value : selection.label;
         let errors = new InvalidErrors()
-        return html`${createField(this.components, { type: "list", name: "selection", label: this.definition.label, helpText: this.definition.helpText, options } as ListFieldDefinition, value, this.path(), errors, (event: ValueChangedEvent<string>) => this.selectionChanged(event), null)}
+        return html`${createField(this.components, { type: "list", name: "selection", options } as ListFieldDefinition, value, this.path(), errors, (event: ValueChangedEvent<string>) => this.selectionChanged(event), null)}
       <div class="form">${createField(this.components, selection.form, this.value?.value, this.path()+".value", errors, (event: ValueChangedEvent<any>) => this.changed(event), null)}</div>`;
 
       }
