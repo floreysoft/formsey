6c001de5346e507049578fdcece54de309e339d8
Alexander Brickwedde <alex_git@brickwedde.de>
2020-07-31 16:21:53 +0200

    Placeholder when image load fails + markup width fix

--- packages/formsey-fields-native/ImageField.ts
+++ packages/formsey-fields-native/ImageField.ts
@@ -1,12 +1,26 @@
-import { html, property, css } from 'lit-element';
+import { html, property, query } from 'lit-element';
 import { ImageFieldDefinition, LabeledField, register } from '@formsey/core';
 
 export class ImageField extends LabeledField<ImageFieldDefinition, string> {
   @property({ converter: Object })
   definition: ImageFieldDefinition;
 
+  @query("img")
+  img: HTMLImageElement
+
+  errorOccured = false;
+
   renderField() {
-    return html`<div class="if" style="text-align: ${this.definition.align}"><img src="${this.definition.url}" title="${this.definition.label}" style="width: ${this.definition.width}"></div>`
+    return html`<div class="if" style="text-align: ${this.definition.align}"><img src="${this.definition.url}" title="${this.definition.label}" @error="${this.loaderror}" style="width: ${this.definition.width}"></div>`
+  }
+
+  loaderror(e : Event) {
+    if (!this.errorOccured) {
+      this.errorOccured = true;
+      this.img.src = "//formsey.com/assets/images/formsey.png";
+      this.img.style.maxHeight = "100px";
+      this.img.style.maxWidth = "100px";
+    }
   }
 }
 register("formsey-image", ImageField, ["native", "material","vaadin"], "image", { importPath: "@formsey/fields-native/ImageField"})
