9ca970fcad625cee12fbb598c0723a53531c4db0
dflorey <daniel.florey@gmail.com>
2020-08-14 11:04:55 +0200

    load lit es bundle

--- packages/formsey-fields-vaadin/rollup.config.js
+++ packages/formsey-fields-vaadin/rollup.config.js
@@ -3,13 +3,20 @@ import resolve from 'rollup-plugin-node-resolve';
 import minifyHTML from 'rollup-plugin-minify-html-literals';
 
 export default {
-  external: ['lit-element', 'lit-html'],
   input: './index.js',
   output: {
     file: 'esm/formsey-fields-vaadin.min.js',
     sourcemap: false
   },
   plugins: [
+    {
+      resolveId(id, importer) {
+        if (id.startsWith('lit-element') || id.startsWith('lit-html')) {
+          console.log("Import "+id+" from "+importer)
+          return { id: `https://unpkg.com/@floreysoft/lit@0.3.211/esm/lit.min.js?module`, external: true };
+        }
+      }
+    },
     resolve(),
     minifyHTML(),
     terser({
