27effe54639d1e7a12a4f934bba46ce780effbf0
dflorey <daniel.florey@gmail.com>
2020-08-14 11:56:29 +0200

    extracting core into single module

--- packages/formsey-core/rollup.config.js
+++ packages/formsey-core/rollup.config.js
@@ -1,12 +1,11 @@
 import { terser } from 'rollup-plugin-terser';
 import resolve from 'rollup-plugin-node-resolve';
 import minifyHTML from 'rollup-plugin-minify-html-literals';
-import corePackage from '../formsey-core/package.json'
 
 export default {
   input: './index.js',
   output: {
-    file: 'esm/formsey-fields-vaadin.min.js',
+    file: 'esm/formsey-core.min.js',
     sourcemap: false
   },
   plugins: [
@@ -14,9 +13,6 @@ export default {
       resolveId(id, importer) {
         if (id.startsWith('lit-element') || id.startsWith('lit-html')) {
           return { id: `https://unpkg.com/@floreysoft/lit@0.3.211/esm/lit.min.js?module`, external: true };
-        }
-        if (id.startsWith('@formsey/core')) {
-          return { id: `https://unpkg.com/@formsey/core@${corePackage.version}/esm/formsey-core.min.js?module`, external: true };
         }
       }
     },
