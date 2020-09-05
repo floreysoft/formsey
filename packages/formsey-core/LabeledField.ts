c454820a0edd6c0826379df725fddd4f4e248f19
dflorey <daniel.florey@gmail.com>
2020-09-05 15:06:19 +0200

    label field changed

--- packages/formsey-core/LabeledField.ts
+++ packages/formsey-core/LabeledField.ts
@@ -7,7 +7,7 @@ import { unsafeHTML } from 'lit-html/directives/unsafe-html';
 
 export abstract class LabeledField<T extends FieldDefinition, V> extends Field<T, V> {
   protected render(): void | TemplateResult {
-    return html`<div class="${classMap({ lfw: true, lfi: !this.valid && this.report })}">${this.renderHeader()}${this.renderField()}${this.renderFooter()}</div>`
+    return html`<div class="${classMap({ lfw: true, lfi: !this.valid && this.report })}">${this.renderHeader()}${this.renderFooter()}</div>`
   }
 
   protected abstract renderField() : TemplateResult | void
@@ -17,11 +17,7 @@ export abstract class LabeledField<T extends FieldDefinition, V> extends Field<T
     if (this.definition.hasOwnProperty('required')) {
       required = (<InputFieldDefinition>this.definition).required
     }
-    // Optional: Help text in a bubble?
-    // let helpText = this.definition.helpText ? html`<div class="lfhtb">${this.definition.helpText}</div>` : undefined
-    let helpText = undefined
-    return html`
-      ${this.definition.label ? html`<div class="lfl">${this.definition.label}${required ? html`<span class="lfr">&#10033;</span>` : html``}${helpText}</div>` : undefined}`
+    return html`${this.definition.label ? html`<label class="lfl">${this.definition.label}${required ? html`<span class="lfr">&#10033;${this.renderField()}</label>` : html``}</div>` : this.renderField()}`
   }
 
   protected renderFooter(): TemplateResult | void {
@@ -35,14 +31,6 @@ export abstract class LabeledField<T extends FieldDefinition, V> extends Field<T
     }
     let help = this.definition.helpText
     let helpText = help ? html`<div class="lfht">${help}</div>` : undefined
-    /*
-    let helpText
-    if ( typeof help == "string" ) {
-      help = Marked.parse(help)
-      helpText = html`<div class="lfht">${unsafeHTML(help)}</div>`
-    } else {
-    }
-    */
     return !this.valid && this.report && validityMessage ? html`<div class="lfet">${validityMessage}</div>` : helpText
   }
 }
