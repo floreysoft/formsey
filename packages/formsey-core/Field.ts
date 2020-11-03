0a18cd2907dc610a045d6884f99c4ba1d4359180
dflorey <daniel.florey@gmail.com>
2020-11-03 12:26:23 +0100

    removed logs

--- packages/formsey-core/Field.ts
+++ packages/formsey-core/Field.ts
@@ -18,7 +18,7 @@ export const createField = (components: Components, settings: Settings, definiti
       const library = libraries[key]
       const component = library.components[definition.type]
       if ( component ) {
-        console.warn(`Field of type=${definition.type} not found in your components library, returning it from library=${key}}`);
+        // console.debug(`Field of type=${definition.type} not found in your components library, returning it from library=${key}}`);
         return component.factory(components, settings, definition, value, parentPath, errors, changeHandler, invalidHandler, id)
       }
     }
