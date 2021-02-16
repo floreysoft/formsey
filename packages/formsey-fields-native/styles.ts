e24cf1edea4d9fabbd0b1b3c3ac0a7564c437501
dflorey <daniel.florey@gmail.com>
2021-02-16 16:30:47 +0100

    z-index revcerted

--- packages/formsey-fields-native/styles.ts
+++ packages/formsey-fields-native/styles.ts
@@ -296,7 +296,6 @@ export const FORM_STYLES = css`
     display: flex;
     flex-direction: column;
     overflow: hidden;
-    z-index: 1;
   }
   .ffg {
     display: flex;
@@ -373,7 +372,7 @@ export const FORM_STYLES = css`
   }
 
   /* Expand various items */
-  formsey-toggle,formsey-list,formsey-option,formsey-panel,formsey-table,formsey-responsive-panel,formsey-split-panel,formsey-tab-panel,formsey-grid-view,formsey-stats-view,formsey-text,formsey-sourcecode,formsey-signature,formsey-table>.lfw>section, formsey-form-field,formsey-form-field>.lfw>section, formsey-optional-section>section, formsey-selectable-section>section, formsey-repeating-section>section {
+  formsey-toggle,formsey-list,formsey-option,formsey-panel,formsey-table,formsey-responsive-panel,formsey-split-panel,formsey-tab-panel,formsey-grid-view,formsey-stats-view,formsey-text,formsey-sourcecode,formsey-signature,formsey-table>.lfw>section, formsey-form-field,formsey-form-field-designer,formsey-form-field>.lfw>section, formsey-optional-section>section, formsey-selectable-section>section, formsey-repeating-section>section {
     display: flex;
     position:relative;
     flex-direction: column;
