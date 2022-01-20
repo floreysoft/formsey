b5472a1cbca7bc952e6966dfadfed59167fec272
Alexander Brickwedde <alex.brickwedde@floreysoft.net>
2022-01-20 11:19:23 -0500

    Added type to Intl.DateTimeFormat

--- packages/formsey-fields-native/LabelField.ts
+++ packages/formsey-fields-native/LabelField.ts
@@ -16,7 +16,7 @@ export class LabelField extends LabeledField<LabelFieldDefinition, any> {
         formatted = new Intl.NumberFormat(this.definition.locale || navigator.language, { style: this.definition.style.selection, currency: this.definition.style.value.currency, currencyDisplay: this.definition.style.value.currencyDisplay, currencySign: this.definition.style.value.currencySign, useGrouping: this.definition.useGrouping, signDisplay: this.definition.signDisplay, unit: this.definition.style.value.unit, unitDisplay: this.definition.style.value.unitDisplay }).format(this.value)
       } else if (this.definition.format == "datetime") {
         const date = new Date(this.value)
-        formatted = new Intl.DateTimeFormat(this.definition.locale || navigator.language, { dateStyle: this.definition.dateStyle, timeStyle: this.definition.timeStyle }).format(date)
+        formatted = new Intl.DateTimeFormat(this.definition.locale || navigator.language, { dateStyle: this.definition.dateStyle, timeStyle: this.definition.timeStyle } as Intl.DateTimeFormatOptions).format(date)
       }
       return html`<div class="${classMap({ l: true, "w": this.definition.wrap == "wrap" ? true : false })}"><span>${formatted}</span></div>`
     }
